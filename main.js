24746ea314ec14b58c7cf843f3a922fa67cc1e0e
Brandon <99849502+bfreeman12@users.noreply.github.com>
2023-03-10 15:17:37 -0500

    Added copy response to clipboard button

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const apiKeySubmit = document.getElementById("apiKeySubmit");
 const apiKeyInput = document.getElementById("apiKeyInput");
 const queryInput = document.getElementById("queryInput");
 const querySubmit = document.getElementById("querySubmit");
+const toClipboard = document.getElementById("toClipboard");
+const gptResponseP = document.getElementById("gptResponse");
 
 querySubmit.onclick = () => {
   const prefs = {
@@ -15,9 +17,12 @@ chrome.runtime.onMessage.addListener((data) => {
   let responseText = JSON.stringify(data["gptResponseData"]);
   const formattedResponseText = responseText.replaceAll("\\n", "");
   const newformattedResponseText = formattedResponseText.replaceAll("\\", "");
-  const gptResponseP = document.getElementById("gptResponse");
   gptResponseP.innerHTML = newformattedResponseText;
 });
 
 // in the future will need something to hide the api key input and save
 // the api locally
+toClipboard.onclick = () => {
+  const valueToBeCopied = gptResponseP.textContent;
+  navigator.clipboard.writeText(valueToBeCopied);
+};
